6ecd5b78abd9413c6797ab7cbf9ca0e0eb2a515b
James Montemagno <james.montemagno@gmail.com>
2014-06-05 11:39:54 -0700

    Initial Commit

---
+++ iOS/Renderers/PullToRefreshListViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using PullToRefresh.iOS.Renderers;
+using PullToRefresh;
+using MonoTouch.UIKit;
+
+[assembly:ExportRendererAttribute(typeof(PullToRefreshListView), typeof(PullToRefreshListViewRenderer))]
+namespace PullToRefresh.iOS.Renderers
+{
+	public class PullToRefreshListViewRenderer : ListViewRenderer
+	{
+		public PullToRefreshListViewRenderer ()
+		{
+		}
+
+		private FormsUIRefreshControl refreshControl;
+		protected override void OnModelSet(VisualElement model)
+		{
+			base.OnModelSet(model);
+
+			var pullToRefreshListView = (PullToRefreshListView)this.Model; 
+			var tableView = (UITableView) this.Control;
+
+			refreshControl = new FormsUIRefreshControl ();
+			refreshControl.RefreshCommand = pullToRefreshListView.RefreshCommand;
+			refreshControl.Message = pullToRefreshListView.Message;
+			tableView.AddSubview (refreshControl);
+		}
+
+
+		/// <summary>
+		/// Called when the underlying model's properties are changed
+		/// </summary>
+		/// <param name="sender">Model</param>
+		/// <param name="e">Event arguments</param>
+		protected override void OnHandlePropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnHandlePropertyChanged(sender, e);
+
+			var pullToRefreshListView = (PullToRefreshListView)this.Model; 
+	
+
+			if (e.PropertyName == PullToRefreshListView.IsRefreshingProperty.PropertyName) {
+				refreshControl.IsRefreshing = pullToRefreshListView.IsRefreshing;
+			} else if (e.PropertyName == PullToRefreshListView.MessageProperty.PropertyName) {
+				refreshControl.Message = pullToRefreshListView.Message;
+			} else if (e.PropertyName == PullToRefreshListView.RefreshCommandProperty.PropertyName) {
+				refreshControl.RefreshCommand = pullToRefreshListView.RefreshCommand;
+			}
+		}
+	}
+}
+
